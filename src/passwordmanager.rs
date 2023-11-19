0fc34a82fe6f040a9bbbdab06a8baf9cd4fd2c95
Timo Dehandschutter <timo.dehandschutter@gmail.com>
2023-11-19 17:11:56 +0100

    replace encrypted with hashed for sha256

--- src/passwordmanager.rs
+++ src/passwordmanager.rs
@@ -7,7 +7,7 @@ use std::{
 
 use rand::prelude::*;
 
-use dialoguer::{Confirm, Password};
+use dialoguer::{Confirm, Input, Password};
 
 pub fn run() {
     let args: Vec<String> = env::args().collect();
@@ -15,18 +15,28 @@ pub fn run() {
     let command = &args[1];
 
     match command.as_str() {
-        "new" => {
+        "new" | "n" => {
             if args.len() > 2 {
                 new_database(&args[2]);
             } else {
-                println!("Error: no database suplied");
+                let filename: String = Input::new()
+                    .with_prompt("name new database")
+                    .interact_text()
+                    .unwrap();
+
+                new_database(&filename);
             }
         }
-        "open" => {
+        "open" | "o" => {
             if args.len() > 2 {
                 open_database(&args[2])
             } else {
-                println!("Error: no database suplied");
+                let filename: String = Input::new()
+                    .with_prompt("file to open")
+                    .interact_text()
+                    .unwrap();
+
+                open_database(&filename);
             }
         }
         _ => println!("Error: argument not found"),
@@ -50,9 +60,9 @@ fn new_database(filename: &str) {
     let mut rng = rand::thread_rng();
     let salt: i32 = rng.gen();
 
-    let encrypted_password = sha256::digest(format!["{}{}", salt, password]);
+    let hashed_password = sha256::digest(format!["{}{}", salt, password]);
 
-    writeln!(file, "{}", encrypted_password).unwrap();
+    writeln!(file, "{}", hashed).unwrap();
     writeln!(file, "{}", salt).unwrap();
 
     println!("Succesfully created new database!");
@@ -75,7 +85,7 @@ fn open_database(filename: &str) {
         return;
     }
 
-    let encrypted_password = lines.next().unwrap();
+    let hashed_password = lines.next().unwrap();
     let salt = lines.next().unwrap();
 
     print!("Enter password: ");
@@ -86,9 +96,9 @@ fn open_database(filename: &str) {
         .interact()
         .unwrap();
 
-    let input_encrypted_password = sha256::digest(format!["{}{}", salt, input_password]);
+    let input_hashed_password = sha256::digest(format!["{}{}", salt, input_password]);
 
-    if input_encrypted_password == encrypted_password {
+    if input_hashed_password == hashed_password {
         println!("correct password");
     } else {
         println!("incorrect password");
@@ -107,5 +117,3 @@ fn ask_to_create(filename: &str) -> bool {
 
     false
 }
-
-// fn ask_to_create()
